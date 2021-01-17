ff915d0ffc8a41387ddb8ac25eb7964d9443ca70
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-01-17 12:42:22 +0330

    type conversion

--- home.js
+++ home.js
@@ -1,117 +1,161 @@
-// https://hackr.io/blog/javascript-cheat-sheet
-// storing values with assignment operator
-var a;
-var b = 2;
-console.log(a);
-a = 7;
+// // https://hackr.io/blog/javascript-cheat-sheet
+// // storing values with assignment operator
+// var a;
+// var b = 2;
+// console.log(a);
+// a = 7;
 
-b = a;
+// b = a;
 
-console.log(a);
-console.log(b);
-
-
-// uninitialized variables
-var a = 5;
-var b = 10;
-var c = 'I am a';
-
-a = a + 1;
-b = b + 5;
-c = c + ' string';
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// case sensitiveity in variables
-var stydlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-STUDLYCAPVAR = 10;
-ProperCamelCase = "a string";
-TitleCASEOver = 1000;
-
-console.log(ProperCamelCase);
-console.log(properCamelCase);
-
-// adding number
-var sum = 10 + 10;
-console.log(sum);
-
-// subtracting numbers
-var difference = 10 - 3;
-console.log(difference);
-
-// multiplying numbers
-var product = 10 * 3;
-console.log(product);
-
-// diving number;
-var div = 10 / -0;
-console.log(div);
-
-// incrementing number
-var myVar = 20;
-myVar = myVar + 1;
-myVar ++;
-console.log(myVar);
-
-// decrementing number
-var myVar = 20;
-myVar = myVar - 1;
-myVar --;
-console.log(myVar);
-
-// reminder
-var reminder;
-reminder = 11 % 3;
-console.log(reminder);
-
-// compound assignment with augmented addition
-var a = 5;
-var b = 10;
-var c = 'I am a';
-
-a += 1;
-b -= 5;
-c += ' string';
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// scape string litral
-var myStr = "I am a \\\"double quoted\" string inside \"double quotes\"";
-console.log(myStr);
-
-// scape qouting string with single qoutes
-var myStr = '"I am a "double quoted" string inside "double quotes"';
-console.log(myStr);
-
-var myStr = `"I am a "double quoted" string inside 'double quotes""`;
-console.log(myStr);
+// console.log(a);
+// console.log(b);
 
 
-// string length
-var myStr = 'mojtaba';
-console.log(myStr.length);
+// // uninitialized variables
+// var a = 5;
+// var b = 10;
+// var c = 'I am a';
 
-// using []
-console.log(myStr[0]);
-console.log(myStr[2]);
+// a = a + 1;
+// b = b + 5;
+// c = c + ' string';
 
-// find last item
-console.log(myStr[myStr.length - 1]);
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-// find nth from last
-console.log(myStr[myStr.length - 4]);
-// if else
+// // case sensitiveity in variables
+// var stydlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
 
-// for loop
+// STUDLYCAPVAR = 10;
+// ProperCamelCase = "a string";
+// TitleCASEOver = 1000;
 
-// while loop
+// console.log(ProperCamelCase);
+// console.log(properCamelCase);
 
-// function
+// // adding number
+// var sum = 10 + 10;
+// console.log(sum);
+
+// // subtracting numbers
+// var difference = 10 - 3;
+// console.log(difference);
+
+// // multiplying numbers
+// var product = 10 * 3;
+// console.log(product);
+
+// // diving number;
+// var div = 10 / -0;
+// console.log(div);
+
+// // incrementing number
+// var myVar = 20;
+// myVar = myVar + 1;
+// myVar ++;
+// console.log(myVar);
+
+// // decrementing number
+// var myVar = 20;
+// myVar = myVar - 1;
+// myVar --;
+// console.log(myVar);
+
+// // reminder
+// var reminder;
+// reminder = 11 % 3;
+// console.log(reminder);
+
+// // compound assignment with augmented addition
+// var a = 5;
+// var b = 10;
+// var c = 'I am a';
+
+// a += 1;
+// b -= 5;
+// c += ' string';
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // scape string litral
+// var myStr = "I am a \\\"double quoted\" string inside \"double quotes\"";
+// console.log(myStr);
+
+// // scape qouting string with single qoutes
+// var myStr = '"I am a "double quoted" string inside "double quotes"';
+// console.log(myStr);
+
+// var myStr = `"I am a "double quoted" string inside 'double quotes""`;
+// console.log(myStr);
+
+
+// // string length
+// var myStr = 'mojtaba';
+// console.log(myStr.length);
+
+// // using []
+// console.log(myStr[0]);
+// console.log(myStr[2]);
+
+// // find last item
+// console.log(myStr[myStr.length - 1]);
+
+// // find nth from last
+// console.log(myStr[myStr.length - 4]);
+
+// Implicit Type Conversion
+
+// const name = 'mojtaba'
+// const lastName = 'aminzadeh'
+// const fullName = name + ' ' + lastName
+// console.log(fullName);
+
+// const number = 4;
+// const number2 = 10;
+// const result = number + number2;
+// console.log(result);
+
+// const value = number / lastName;
+// console.log(value);
+
+// let number3 = 10;
+// let number4 = '5';
+// const result2 = number3 + number4;
+// console.log(result2);
+
+const randomNumber = 5;
+document.querySelector('.form').addEventListener('submit', function (e) {
+ e.preventDefault();
+ let value = document.getElementById('amount').value;
+ // console.log(typeof(value));
+ value = parseInt(value);
+ console.log('input value: ', value);
+ console.log('sum of value: ', randomNumber + value);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // if else
+
+// // for loop
+
+// // while loop
+
+// // function
 
