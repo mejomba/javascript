9c992057330ed8487f531c83aca5490ade615253
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-01-27 18:02:21 +0330

    string methods

--- section2.js
+++ section2.js
@@ -5,7 +5,24 @@
 
 // string properties and methods
 // don't memorize methods
-let text = 'mojtaba aminzadeh'
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// https://www.w3schools.com/js/js_string_methods.asp
+
+let text = ' Mojtaba Aminzadeh'
+let result = text.length
+console.log(result);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(1));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('Aminzadeh'));
+console.log(text);
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith('mojtaba'));
+console.log(text.includes('amin'));
+console.log(text.slice(2, 3));
+console.log(text.slice(-5));
+
 
 
 // const person = {
