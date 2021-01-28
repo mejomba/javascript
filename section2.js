9e53cb5cd9f9b1af32d1952593faf59cd95827dc
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-01-29 00:42:28 +0330

    array methods

--- section2.js
+++ section2.js
@@ -56,3 +56,38 @@
 // Array Properties and Methods
 let names = ['john', 'bobo', 'barry', 'ben'];
 
+// lenght
+console.log(names.length);
+console.log(names[names.length - 1]);
+
+// concat
+const lastNames = ['pepper', 'onion', 'banana'];
+const allNames = names.concat(lastNames);
+console.log(allNames);
+
+// reverse
+console.log(allNames.reverse());
+
+// unshift
+allNames.unshift('susy');
+allNames.unshift('anna');
+console.log(allNames);
+// shift
+allNames.shift();
+allNames.shift();
+console.log(allNames);
+
+// push
+allNames.push('susy');
+console.log(allNames);
+
+// pop
+allNames.pop();
+allNames.pop();
+allNames.pop();
+console.log(allNames);
+
+// splice - mutates original array
+const newArray = allNames.splice(2, 1);
+console.log(newArray);
+console.log(allNames);
