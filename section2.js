c7bee916887e1cd0e9867338df4f6fa7dbefaefd
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-01-28 17:44:42 +0330

    template literal

--- section2.js
+++ section2.js
@@ -1,29 +1,27 @@
 // string Methods
 // arary iterators - (map, filter, reduce)
 // global objects - Math, Date
-// =====================
+// ===================== 1
 
 // string properties and methods
 // don't memorize methods
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 // https://www.w3schools.com/js/js_string_methods.asp
 
-let text = ' Mojtaba Aminzadeh'
-let result = text.length
-console.log(result);
-console.log(text.toUpperCase());
-console.log(text.toLowerCase());
-console.log(text.charAt(1));
-console.log(text.charAt(text.length - 1));
-console.log(text.indexOf('Aminzadeh'));
-console.log(text);
-console.log(text.trim());
-console.log(text.trim().toLowerCase().startsWith('mojtaba'));
-console.log(text.includes('amin'));
-console.log(text.slice(2, 3));
-console.log(text.slice(-5));
-
-
+// let text = ' Mojtaba Aminzadeh'
+// let result = text.length
+// console.log(result);
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.charAt(1));
+// console.log(text.charAt(text.length - 1));
+// console.log(text.indexOf('Aminzadeh'));
+// console.log(text);
+// console.log(text.trim());
+// console.log(text.trim().toLowerCase().startsWith('mojtaba'));
+// console.log(text.includes('amin'));
+// console.log(text.slice(2, 3));
+// console.log(text.slice());
 
 // const person = {
 //  name: 'mojtaba', // property
@@ -35,3 +33,26 @@ console.log(text.slice(-5));
 // console.log(person);
 // console.log(person.name);
 // person.hello();
+
+
+
+// ================================== 2
+// Template Literals - ES6+
+// Backtick characters ``
+// Interpolation ${} - insert expresion(value)
+
+// const name = 'mojtaba';
+// const age = 27;
+// const sentence = "hey i'm " + name + " and i have " + age + " years old";
+
+// const value = `hey i'm ${name} and i have ${age} years old ${2 + 2}`;
+
+// console.log(sentence);
+// console.log(value);
+
+
+
+// ================================== 3
+// Array Properties and Methods
+let names = ['john', 'bobo', 'barry', 'ben'];
+
