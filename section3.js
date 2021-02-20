1f3c13834e42dc65b68b2636922d20db083dbd3a
Mojtaba Aminzadeh <moojaminzadeh@gmail.com>
2021-02-20 15:06:21 +0330

    mouse

--- section3.js
+++ section3.js
@@ -218,9 +218,42 @@
 // select element
 // addEventListener()
 // what event, what to do
+// const btn = document.querySelector('.btn');
+// const heading = document.querySelector('h2');
+
+// btn.addEventListener('click', function() {
+//  let hasClass = heading.classList.contains('red');
+//  if (hasClass) {
+//   heading.classList.remove('red');
+//  }else {
+//   heading.classList.add('red');
+//  }
+// })
+
+
+
+// ============================== 13
+// click
+// mousedown - button is pressed
+// mouseup - button is released
+// mouseenter - moved onto an element
+// mouseleave - moved out of an element
 const btn = document.querySelector('.btn');
 const heading = document.querySelector('h2');
 
 btn.addEventListener('click', function() {
+ console.log('you click me');
+})
+btn.addEventListener('mousedown', function() {
+ console.log('down');
+})
+btn.addEventListener('mouseup', function() {
+ console.log('up');
+})
+
+heading.addEventListener('mouseenter', function() {
  heading.classList.add('red');
+})
+heading.addEventListener('mouseleave', function() {
+ heading.classList.remove('red');
 })
